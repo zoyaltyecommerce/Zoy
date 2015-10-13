aa6f5da3030691ac9dc6bcd2f260b7b5022b0e7a
zoyaltyecommerce <zoyalty@linkskart.com>
2015-10-13 13:02:03 +0530

    email

--- Zoyal/Zoyal/ContactUs.aspx.cs
+++ Zoyal/Zoyal/ContactUs.aspx.cs
@@ -5,6 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Zoyal.Code;
+using System.Data;
+using System.Net;
+using System.Net.Mail;
+using System.Drawing;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace Zoyal
 {
@@ -53,6 +60,8 @@ namespace Zoyal
                 obj.CONTACT_MODIFIEDBY = 1;
                 obj.CONTACT_PHONENUMBER = txt_phonenumber.Text;
                 obj.CONTACT_SUBJECT = BLL.ReplaceQuote(txt_subject.Text);
+                DataTable dt = BLL.CONTACT_EMAIL(obj);
+                DataTable dt_contact = new DataTable();
                 bool status = BLL.INSERTCONTACT(obj);
                 if (status == true)
                 {
@@ -62,7 +71,31 @@ namespace Zoyal
                 else
                 {
                     BLL.ShowMessage(this, "Please contact administrator");
+                    clearcontrols();
                 }
+
+                MailMessage mailmessage = new MailMessage();
+                mailmessage.IsBodyHtml = true;
+
+                SmtpClient client = new SmtpClient("linkskart.com");
+                client.Credentials = new System.Net.NetworkCredential("info@linkskart.com", ".santhu143");
+                mailmessage.From = new System.Net.Mail.MailAddress("info@linkskart.com");
+                // mailmessage.From = new MailAddress("santhosh@pragatipadh.com");
+                mailmessage.To.Add(dt_contact.Rows[0]["CONTACT_EMAIL"].ToString());
+                // mailmessage.CC.Add(emailid);
+                mailmessage.Subject = "your account is created";
+                mailmessage.Body = "<p> Dear " + dt_contact.Rows[0]["CONTACT_EMAIL"].ToString() + " " + ",<br /> <br />Your account is successfully created "  + " please <a href=\"http://www.linkskart.com\">Click Here</a> to visit LINKSKART.</p></div>";
+                client.EnableSsl = false;
+                try
+                {
+                    client.Send(mailmessage);
+                    //SmtpMail.Send(eMail);
+                }
+                catch (Exception ae)
+                {
+                    // Label1.Text = ae.Message;
+                }
+
             }
             catch (Exception ex)
             {
